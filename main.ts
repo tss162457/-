93a2073be544eec4197bfb6d2f1cb4daeae94dd8
tss162457 <61876129+tss162457@users.noreply.github.com>
2020-07-19 13:09:37 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    if (direction == 1 && state == 0) {
+    	
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+let direction = 0
+let state = 0
+state = 1
+direction = 1
